130b7243f9cd98dab2122adb9466f76903f2fc40
SerenityUX <thomas@serenidad.app>
2024-11-07 15:14:25 -0800

    new fav

--- pages/index.js
+++ pages/index.js
@@ -718,7 +718,7 @@ useEffect(() => {
   return (
     <>
       <Head>
-        <title>Hack Mind</title>
+        <title>Always</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 <meta name="description" content=""/>
