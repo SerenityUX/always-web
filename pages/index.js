184e181a679bf4ab35811573075d572336792488
SerenityUX <thomas@serenidad.app>
2024-11-09 10:24:38 -0800

    Open Graph

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,17 @@ export default function Landing() {
         <title>Always</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Always - Up-to-date Run of Show" />
+        <meta property="og:description" content="Keep your run of show always in your team's hands" />
+        <meta property="og:image" content="/OpenGraph.png" />
+        <meta property="og:url" content="https://always.so" />
+        <meta property="og:type" content="website" />
+        
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/OpenGraph.png" />
       </Head>
       {hasLoadedIfHasToken &&
       <div style={{
