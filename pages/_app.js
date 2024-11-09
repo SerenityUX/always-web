da209409d671cf3a5ad60cdf12fbee5f412bb37c
SerenityUX <thomas@serenidad.app>
2024-11-09 10:36:20 -0800

    see if fixes meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import "../styles/globals.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Always</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Always - Up-to-date Run of Show" />
+        <meta property="og:description" content="Keep your run of show always in your team's hands" />
+        <meta property="og:image" content="https://always.sh/OpenGraph.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="https://always.sh" />
+        <meta property="og:type" content="website" />
+        
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://always.sh/OpenGraph.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
